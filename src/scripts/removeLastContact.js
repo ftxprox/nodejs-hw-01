4346e892808165307f335c61e61778a03c765d22
HryhorievOA <ftxprox@gmail.com>
2025-01-08 18:43:52 +0200

    ppc2

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const data = JSON.parse(await readContacts());
+    if (data.length > 0) {
+      data.pop();
+      writeContacts(data);
+    } else {
+      return data;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 removeLastContact();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from './createFakeContact.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts = createFakeContact()) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts), 'utf-8');
+    console.log('Data written succesfully');
+  } catch (e) {
+    console.error(e);
+  }
+};
