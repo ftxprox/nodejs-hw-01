34bc641b5a3cc201d4a260ba1ca319848bb4b686
HryhorievOA <ftxprox@gmail.com>
2025-01-30 21:10:17 +0200

    ppc

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -2,11 +2,8 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 
 export const readContacts = async () => {
-  try {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
-    return data ? data : '[]';
-  } catch (e) {
-    console.error(e);
-    return '[]';
-  }
+
+  const data = await fs.readFile(PATH_DB, 'utf-8');
+
+  return JSON.parse(data);
 };
